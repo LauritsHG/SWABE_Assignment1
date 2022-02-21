7332d80e26e63d10d6872b521324dbfb3a2dfa17
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2022-02-21 22:33:56 +0100

    fix

--- src/models/UserSchema.ts
+++ src/models/UserSchema.ts
@@ -1,17 +1,37 @@
 import { Schema } from "mongoose";
+import { DIGEST, ITERATIONS, KEY_LENGTH, pbkdf2 } from "../util/crypto-util";
 
+export enum Role {
+  manager = "MANAGER",
+  clerk = "CLERK",
+  guest = "GUEST",
+}
 export interface User {
-  firstName: String;
-  lastName: String;
-  email: String;
-  password: String;
-  salt: String;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: Role;
+  password: string;
+  salt: string;
+  isPasswordValid(password: string): boolean;
 }
 
 export const schema = new Schema<User>({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
+  role: { type: String, required: true },
   password: { type: String, required: true },
   salt: { type: String, required: true },
 });
+
+schema.methods.isPasswordValid = async function (password: string) {
+  const hash = await pbkdf2(
+    password,
+    this.salt,
+    ITERATIONS,
+    KEY_LENGTH,
+    DIGEST
+  );
+  return this.hash === hash.toString("hex");
+};
